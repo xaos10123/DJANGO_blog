7bd3bfce7be8b01c3e198eb2c414c00947300654
xaos10123 <xaos10123@gmail.com>
2024-12-16 22:30:10 +0700

    hm_21: base template for main page, add settings

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from python_blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='home'),
+    
 ]
--- python_blog/views.py
+++ python_blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+CATEGORIES = [
+    {'slug': 'python', 'name': 'Python'},
+    {'slug': 'django', 'name': 'Django'},
+    {'slug': 'postgresql', 'name': 'PostgreSQL'},
+    {'slug': 'docker', 'name': 'Docker'},
+    {'slug': 'linux', 'name': 'Linux'},
+]
+
+def main(request):
+    context = {
+        'data': CATEGORIES,
+    }
+    return render(request, 'main.html', context=context)
+
