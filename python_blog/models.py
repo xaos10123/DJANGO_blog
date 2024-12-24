f2808704fbaacb63fad12e27ab6458a63c3dff4d
xaos10123 <xaos10123@gmail.com>
2024-12-24 13:54:46 +0700

    hw_23: создана первая модель Post

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    data = models.JSONField(blank=True, null=True, default=list)
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return f'/post/{self.slug}'
+    
