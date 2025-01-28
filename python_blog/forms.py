74af85e15a7b61aef25775f846fdfdc1a156af45
xaos10123 <xaos10123@gmail.com>
2025-01-28 15:02:18 +0700

    hw:26 Создание формы

--- python_blog/forms.py
+++ python_blog/forms.py
@@ -1,5 +1,23 @@
+from pyexpat import model
 from django import forms
+from django.forms import widgets
+
+from python_blog.models import Category, Post
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False)
     s_from = forms.CharField(max_length=100, required=False)
+
+class PostForm(forms.ModelForm):
+    model = Post
+    fields = ['title', 'content', 'category', 'tags']
+    widgets  = {
+        'title': forms.TextInput(attrs={'class': 'form-control'}),
+        'content': forms.Textarea(attrs={'class': 'form-control'}),
+        'category': forms.Select(attrs={'class': 'form-control'}, choices=Category.objects.all()),
+        'tags': forms.Textarea(attrs={'class': 'form-control'}),
+    }
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'category', 'tags']
