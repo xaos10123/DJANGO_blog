bd07bd77373e9cf3677c72d00c753790546a7b4d
xaos10123 <xaos10123@gmail.com>
2025-02-11 18:25:32 +0700

    hw_28: Регистрация путей

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from users.views import MyLoginView, MyLogoutView, MyRegisterView
+from users.views import (
+    MyLoginView,
+    MyLogoutView,
+    MyRegisterView,
+    ProfileDetailView,
+    ProfileEditView,
+    ProfilePasswordView,
+)
 
 app_name = "user"
 
@@ -8,5 +15,11 @@ urlpatterns = [
     path("login/", MyLoginView.as_view(), name="login"),
     path("logout/", MyLogoutView.as_view(), name="logout"),
     path("register/", MyRegisterView.as_view(), name="register"),
-
+    path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile"),
+    path("profile/<int:pk>/edit/", ProfileEditView.as_view(), name="profile_edit"),
+    path(
+        "profile/<int:pk>/password/",
+        ProfilePasswordView.as_view(),
+        name="profile_password",
+    ),
 ]
