78a822e3951afaca88fefea91f71c10c8be8f2ea
xaos10123 <xaos10123@gmail.com>
2025-02-11 18:25:14 +0700

    hw_28: Создание форм и представлений

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
@@ -30,3 +31,29 @@ class RegisterForm(UserCreationForm):
             attrs={"class": "form-control"}
         )
         self.fields["password2"].label = "Повторите пароль"
+
+
+class ProfileEditForm(forms.ModelForm):   
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+        }
+
+
+class ProfilePasswordForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["old_password"].widget = forms.PasswordInput(
+            attrs={"class": "form-control"}
+        )
+        self.fields["new_password1"].widget = forms.PasswordInput(
+            attrs={"class": "form-control"}
+        )
+        self.fields["new_password2"].widget = forms.PasswordInput(
+            attrs={"class": "form-control"}
+        )
--- users/views.py
+++ users/views.py
@@ -1,14 +1,23 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from users.forms import LoginForm, RegisterForm
+from users.forms import LoginForm, ProfileEditForm, ProfilePasswordForm, RegisterForm
+
+
+class OwnerPermissionMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["is_owner"] = self.request.user == self.get_object()
+        return context
 
 
 class MyLoginView(LoginView):
     template_name = "users/login.html"
     form_class = LoginForm
-    next_page = 'main'
+    next_page = "main"
 
 
 class MyLogoutView(LogoutView):
@@ -19,3 +28,29 @@ class MyRegisterView(CreateView):
     template_name = "users/register.html"
     form_class = RegisterForm
     success_url = reverse_lazy("user:login")
+
+
+class ProfileDetailView(OwnerPermissionMixin, DetailView):
+    model = get_user_model()
+    template_name = "users/profile.html"
+    context_object_name = "user_prof"
+
+
+class ProfileEditView(LoginRequiredMixin, OwnerPermissionMixin, UpdateView):
+    model = get_user_model()
+    template_name = "users/profile_edit.html"
+    form_class = ProfileEditForm
+    login_url = reverse_lazy("user:login")
+
+    def get_success_url(self):
+        return reverse_lazy("user:profile", kwargs={"pk": self.object.pk})
+
+
+class ProfilePasswordView(LoginRequiredMixin, PasswordChangeView):
+    model = get_user_model()
+    template_name = "users/profile_change_password.html"
+    form_class = ProfilePasswordForm
+    login_url = reverse_lazy("user:login")
+
+    def get_success_url(self):
+        return reverse_lazy("user:profile", kwargs={"pk": self.request.user.pk})
