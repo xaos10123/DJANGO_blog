0dc3838f0c3d327f9111c220be8a6dd80ce1aa62
xaos10123 <xaos10123@gmail.com>
2025-02-11 22:29:56 +0700

    Добавлена система уведомлений

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
@@ -19,9 +20,23 @@ class MyLoginView(LoginView):
     form_class = LoginForm
     next_page = "main"
 
+    def form_valid(self, form):
+        messages.success(self.request, f"Добро пожаловать, {form.get_user().username}!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Неверные данные!")
+        return super().form_invalid(form)
+
 
 class MyLogoutView(LogoutView):
+    template_name = "logout.html"
     next_page = "main"
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, "Вы вышли из системы!")
+        return super().post(request, *args, **kwargs)
+    
 
 
 class MyRegisterView(CreateView):
@@ -29,11 +44,21 @@ class MyRegisterView(CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy("user:login")
 
+    def form_valid(self, form):
+        messages.success(self.request, "Вы успешно зарегистрировались!")
+        return super().form_valid(form)
+
 
 class ProfileDetailView(OwnerPermissionMixin, DetailView):
     model = get_user_model()
     template_name = "users/profile.html"
     context_object_name = "user_prof"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user != self.get_object():
+            messages.error(self.request, "Страница доступна только для просмотра!")
+        return context
 
 
 class ProfileEditView(LoginRequiredMixin, OwnerPermissionMixin, UpdateView):
@@ -42,6 +67,14 @@ class ProfileEditView(LoginRequiredMixin, OwnerPermissionMixin, UpdateView):
     form_class = ProfileEditForm
     context_object_name = "user_prof"
     login_url = reverse_lazy("user:login")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Данные успешно изменены!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Неверные данные")
+        return super().form_invalid(form)
 
     def get_success_url(self):
         return reverse_lazy("user:profile", kwargs={"pk": self.object.pk})
@@ -52,6 +85,10 @@ class ProfilePasswordView(LoginRequiredMixin, PasswordChangeView):
     template_name = "users/profile_change_password.html"
     form_class = ProfilePasswordForm
     login_url = reverse_lazy("user:login")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Пароль успешно изменен!")
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy("user:profile", kwargs={"pk": self.request.user.pk})
